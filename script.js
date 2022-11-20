328fedf0bdc8946605bef7a25f3fb0f8bfdedd90
Naman Uttamchandani <naman.uttam7@gmail.com>
2022-11-20 01:03:27 -0800

    Fix bug that does not allow script to run

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const queue = [];
 
-const list = document.body.querySelectro("ol");
+const list = document.getElementById("queue");
 
 const addToQueue = document.getElementById("push");
 const removeFromQueue = document.getElementById("shift");
 
 addToQueue.addEventListener("click", () => {
     const item = document.createElement("li");
-    item.textContent = "This is a new person in the queue";
+    item.textContent = "This is a new person in the queue at position: " + (list.childElementCount + 1);
     list.appendChild(item);
     queue.push(item);
+    console.log("Queue Array:", queue);
 })
 
 removeFromQueue.addEventListener("click", () => {
-    list.removeChild(queue.shift());
+    list.removeChild(list.firstElementChild);
+    queue.shift();
+    console.log("Queue Array:", queue);
 })
 
-console.log("hi");
 
-//Using an arraym with only the methods, push and shift,
+
+//Using an array with only the methods, push and shift,
 //then I can treat that array as a Queue that accepts any element.
