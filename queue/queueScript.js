57b05e80aa2330e91d636368ac32476b31013961
Naman Uttamchandani <naman.uttam7@gmail.com>
2022-11-24 12:28:57 -0800

    Finish Queue + Stack implmentation

--- queue/queueScript.js
+++ queue/queueScript.js
@@ -4,20 +4,34 @@ const list = document.getElementById("queue");
 
 const addToQueue = document.getElementById("push");
 const removeFromQueue = document.getElementById("shift");
+const textArea = document.getElementById("name");
+const display = document.getElementById('display');
 
 addToQueue.addEventListener("click", () => {
+    if (textArea.value === null || textArea.value === "") {
+        alert("Hey you didn't name your person :(");
+        return;
+    }
+
     const item = document.createElement("li");
-    item.textContent = "This is a new person in the queue at position: " + (list.childElementCount + 1);
+
+    const name = textArea.value;
+    textArea.value = "";
+    item.textContent = name;
+
     list.appendChild(item);
     queue.push(item);
     console.log("Queue Array:", queue);
-})
+});
 
 removeFromQueue.addEventListener("click", () => {
     list.removeChild(list.firstElementChild);
+    display.textContent = `It's is ${queue[0].textContent}'s turn`;
     queue.shift();
     console.log("Queue Array:", queue);
-})
+});
+
+
 
 
 
---
+++ stack/stackScript.js
@@ -0,0 +1,21 @@
+const addStack = document.getElementById('push');
+const removeStack = document.getElementById('pop');
+const stack = document.querySelector('ol');
+const text = document.getElementById('name');
+const display = document.getElementById('display');
+
+addStack.addEventListener('click', () => {
+    if (text.value === "" || text.value === null) {
+        alert("Hey you didn't name your plate :(");
+        return;
+    }
+    let item = document.createElement('li');
+    item.textContent = text.value;
+    stack.appendChild(item);
+    text.value = "";
+});
+
+removeStack.addEventListener('click', () => {
+    display.textContent = `${stack.lastElementChild.textContent} was popped off the stack`;
+    stack.removeChild(stack.lastElementChild);
+})
